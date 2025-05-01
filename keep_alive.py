c2a7cef1980191b058358393c459cf539d5805d7
AmanBotz <amanpooniaff@gmail.com>
2025-05-01 15:41:41 +0530

    update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,23 +1,17 @@
-# keep_alive.py
 import asyncio
 import aiohttp
 from info import URL
 
 async def visit_url():
-    if not URL:
-        print("Error: URL not set")
-        return
-    async with aiohttp.ClientSession() as session:
+    if URL:
         try:
-            async with session.get(URL, timeout=10) as resp:
-                print(f"Visited {URL} - Status Code: {resp.status}")
-        except Exception as e:
-            print(f"Error visiting {URL}: {e}")
+            async with aiohttp.ClientSession() as s, s.get(URL, timeout=10) as _: pass
+        except: pass
 
 async def keep_alive():
     while True:
         await visit_url()
-        await asyncio.sleep(60)
+        await asyncio.sleep(30)
 
 if __name__ == "__main__":
     asyncio.run(keep_alive())
